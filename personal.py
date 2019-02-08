5e3312ccabe4520799b221b2753fa94ee1be6fc4
Zach Dwiel <zdwiel@gmail.com>
2019-02-08 13:41:36 -0800

    personal

--- personal.py
+++ personal.py
@@ -7,7 +7,7 @@ ctx.keymap({
     'my username': 'zdwiel',
     'my e-mail': 'zach@dwiel.net',
     'my google e-mail': 'zdwiel@gmail.com',
-    'my intel e-mail': 'zach.dwiel@intel.com',
+    'my (intel | work) e-mail': 'zach.dwiel@intel.com',
     'my last name': 'dwiel',
     'my address': '408 W Howe',
 })
